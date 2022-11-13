d6693da456d0f7a276e669e3cd3297bfe4b03e5c
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-11-13 12:09:32 -0500

    Added port

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,6 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => { console.log("user disconnected") });
 });
 
-server.listen(3000, () => {
-    console.log("Listening on port 3000");
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port", process.env.PORT || 3000);
 });
